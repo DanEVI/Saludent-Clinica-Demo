f768d8eb6e55a81d7e6d6040e0eed521464b215a
DanEvi <villavicenciod139@gmail.com>
2026-09-23 00:09:54 -0500

    add: team page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Precios } from './precios/precios';
+import { Equipo } from './equipo/equipo';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'precios', component: Precios },
+  { path: 'equipo', component: Equipo },
 ];
